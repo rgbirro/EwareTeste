8cf1e6c8aeae79c9ac30a77521285237f76a619c
rgbirro <rgbirro@gmail.com>
2022-04-18 01:35:26 -0300

    Commit com o Readme e com os testes funcionando.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EwareTesteContext>();
 
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
@@ -20,11 +20,11 @@ builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
 
 app.UseHttpsRedirection();
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EwareTeste
+
+## Requisitos
+Visual Studio 2022 Community
+
+## Para Executar
+Ctrl + F5 após carregar o projeto no VS 2022
+Uma janela será aberta com o endereço: https://localhost:7226/ 
+
+## Testes
+no PS ou no terminal digitar o comando: dotnet test
